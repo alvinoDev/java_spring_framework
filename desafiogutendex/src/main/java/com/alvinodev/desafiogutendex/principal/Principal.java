8f264d0cb21593b47d63f4304ac37a3a8069cd3c
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 12:38:56 -0400

    feat: listar top 10 libros mas descargados

--- desafiogutendex/src/main/java/com/alvinodev/desafiogutendex/principal/Principal.java
+++ desafiogutendex/src/main/java/com/alvinodev/desafiogutendex/principal/Principal.java
@@ -1,8 +1,12 @@
 package com.alvinodev.desafiogutendex.principal;
 
 import com.alvinodev.desafiogutendex.model.Datos;
+import com.alvinodev.desafiogutendex.model.DatosLibros;
 import com.alvinodev.desafiogutendex.service.ConvierteDatos;
 import com.alvinodev.desafiogutendex.service.consumoAPI;
+
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -11,9 +15,20 @@ public class Principal {
 
     public void muestraMenu() {
         var json = consumoAPI.obtenerDatos(URL_BASE);
-        System.out.println(json);
+        // System.out.println(json);
 
         var datos = conversor.obtenerDatos(json,Datos.class);
-        System.out.println(datos);
+        // System.out.println(datos);
+
+        // ==========| TOP 10 LIBROS MAS DESCARGADOS |==========
+        AtomicInteger counter = new AtomicInteger(1);
+
+        System.out.println("==========| TOP 10 LIBROS MAS DESCARGADOS |==========");
+        datos.resultados().stream().
+                sorted(Comparator.comparing( DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(book -> book.titulo().toUpperCase())
+                .forEach(titulo -> System.out.println(counter.getAndIncrement() + ".- " + titulo));
+
     }
 }
