4d657f1a11df2c56b520e5be68e06d36e9039bb3
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 19:11:22 -0400

    feat: crear stream para calcular el cuadrado de los numeros

--- JavaCollectionsStreams/src/MainStream.java
+++ JavaCollectionsStreams/src/MainStream.java
@@ -5,10 +5,18 @@ public class MainStream {
     void main() {
 
         // LISTA FILTRADA CON nombres que tengan menos de 5 caracteres
-        List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
-        List<String> listaFiltrada = funcionarios.stream()
-                .filter(f -> f.length() <= 5)
+//        List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
+//        List<String> listaFiltrada = funcionarios.stream()
+//                .filter(f -> f.length() <= 5)
+//                .collect(Collectors.toList());
+//        IO.println(listaFiltrada);
+
+        // CALCULAR EL cuadrado de los números
+        List<Integer> numeros = List.of(2, 3, 5, 7, 11);
+        List<Integer> numCuadrado = numeros.stream()
+                .map(n -> n*n)
                 .collect(Collectors.toList());
-        IO.println(listaFiltrada);
+        IO.println(numCuadrado);
+
     }
 }
