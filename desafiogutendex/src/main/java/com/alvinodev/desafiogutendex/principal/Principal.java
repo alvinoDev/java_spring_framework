af670211c466d56311865f85a6f51a0a304760a0
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 13:03:24 -0400

    feat: buscar libro por nombre

--- desafiogutendex/src/main/java/com/alvinodev/desafiogutendex/principal/Principal.java
+++ desafiogutendex/src/main/java/com/alvinodev/desafiogutendex/principal/Principal.java
@@ -6,12 +6,15 @@ import com.alvinodev.desafiogutendex.service.ConvierteDatos;
 import com.alvinodev.desafiogutendex.service.consumoAPI;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private consumoAPI consumoAPI = new consumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
+    private Scanner input = new Scanner(System.in);
 
     public void muestraMenu() {
         var json = consumoAPI.obtenerDatos(URL_BASE);
@@ -29,6 +32,44 @@ public class Principal {
                 .limit(10)
                 .map(book -> book.titulo().toUpperCase())
                 .forEach(titulo -> System.out.println(counter.getAndIncrement() + ".- " + titulo));
+
+        // ===========| BUSQUEDA DE LIBRO POR NOMBRE |==========
+        System.out.println("===========| BUSQUEDA DE LIBRO POR NOMBRE |==========");
+        var tituloLibro = input.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE+"?search="+tituloLibro.replace(" ", "+"));
+        var datosBusqueda = conversor.obtenerDatos(json, Datos.class);
+        Optional<DatosLibros> libroBuscado = datosBusqueda.resultados().stream()
+                .filter(book -> book.titulo().toUpperCase().contains(tituloLibro.toUpperCase()))
+                .findFirst();
+        if(libroBuscado.isPresent()){
+            System.out.println("LIBRO ENCONTRADO: " + libroBuscado.get());
+        }else{
+            System.out.println("LIBRO NO ENCONTRADO");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
