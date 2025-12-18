733312c42ff36850dad9107bcf70d3009a05723f
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 18:11:39 -0400

    feat: mostrar elementos unicos de una lista

--- JavaCollectionsStreams/src/Main.java
+++ JavaCollectionsStreams/src/Main.java
@@ -1,23 +1,36 @@
 void main() {
 
     // AGREGAR ELEMENTOS A UNA LISTA
-    List<String> empleados = new ArrayList<>();
-    empleados.add("Juan");
-    empleados.add("María");
-    empleados.add("Carlos");
-    empleados.add("Ana");
+//    List<String> empleados = new ArrayList<>();
+//    empleados.add("Juan");
+//    empleados.add("María");
+//    empleados.add("Carlos");
+//    empleados.add("Ana");
 
-    IO.println("Lista de empleados INICIAL: " + empleados);
+//    IO.println("Lista de empleados INICIAL: " + empleados);
 
     // ELIMINAR UN ELEMENTO DE LA LISTA
-    empleados.remove("Carlos");
-    IO.println("Lista después de la eliminación: " + empleados);
+//    empleados.remove("Carlos");
+//    IO.println("Lista después de la eliminación: " + empleados);
 
     // ACCEDER A ELEMENTOS
-    empleados.add("Pedro");
-    empleados.add("Antonio");
+//    empleados.add("Pedro");
+//    empleados.add("Antonio");
 
-    IO.println("La segunda persona de la lista es: " + empleados.get(Integer.hashCode(1)));
-    IO.println("Total de empleados: " + empleados.size());
+//    IO.println("La segunda persona de la lista es: " + empleados.get(Integer.hashCode(1)));
+//    IO.println("Total de empleados: " + empleados.size());
+
+    List<String> eventos = new ArrayList<>();
+    eventos.add("IA Conference");
+    eventos.add("AI Summit");
+    eventos.add("DevFest");
+    eventos.add("Cloud Expo");
+    eventos.add("IA Conference");
+    eventos.add("DevFest");
+
+    IO.println("Lista de eventos: " + eventos);
+
+    Set<String> eventosUnicos = new HashSet<>(eventos);
+    IO.println("Lista de eventos unicos: " + eventosUnicos);
 
 }
