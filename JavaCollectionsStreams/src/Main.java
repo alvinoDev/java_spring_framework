bea20a37359fa2851d9545575a6d175144d1acc3
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 18:01:21 -0400

    feat: agregar elementos a una lista

---
+++ JavaCollectionsStreams/src/Main.java
@@ -0,0 +1,12 @@
+void main() {
+
+    // AGREGAR ELEMENTOS A UNA LISTA
+    List<String> empleados = new ArrayList<>();
+    empleados.add("Juan");
+    empleados.add("María");
+    empleados.add("Carlos");
+    empleados.add("Ana");
+
+    IO.println("Lista de empleados: " + empleados);
+
+}
