f94f6dff05f8663830ffc638310ece8323e99710
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-18 19:07:26 -0400

    feat: crear stream para seleccionar nombres de funcionarios cortos

---
+++ JavaCollectionsStreams/src/MainStream.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+    void main() {
+
+        // LISTA FILTRADA CON nombres que tengan menos de 5 caracteres
+        List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
+        List<String> listaFiltrada = funcionarios.stream()
+                .filter(f -> f.length() <= 5)
+                .collect(Collectors.toList());
+        IO.println(listaFiltrada);
+    }
+}
