6ead1f15dc124b8f8195fb113aec4390ffd95171
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-26 11:58:03 -0400

    Backend: Completando configuración de base de datos en screenmatchpro

---
+++ screenmatchpro/src/main/java/com/alvinodev/screenmatchpro/ScreenmatchproApplication.java
@@ -0,0 +1,23 @@
+package com.alvinodev.screenmatchpro;
+
+import com.alvinodev.screenmatchpro.principal.Principal;
+import com.alvinodev.screenmatchpro.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchproApplication implements CommandLineRunner {
+
+    @Autowired
+    private SerieRepository repository;
+	public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchproApplication.class, args);
+	}
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(repository);
+        principal.muestraElMenu();
+    }
+}
---
+++ screenmatchpro/src/test/java/com/alvinodev/screenmatchpro/ScreenmatchproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinodev.screenmatchpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
