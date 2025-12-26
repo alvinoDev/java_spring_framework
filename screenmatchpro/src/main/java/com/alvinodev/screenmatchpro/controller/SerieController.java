3a9f03fda1525917d24ccc7c9f0eb456412a44e0
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-12-26 15:46:48 -0400

    feat(api-frontend)): crear metodo para devolver una lista de series

--- screenmatchpro/src/main/java/com/alvinodev/screenmatchpro/controller/SerieController.java
+++ screenmatchpro/src/main/java/com/alvinodev/screenmatchpro/controller/SerieController.java
@@ -1,13 +1,20 @@
 package com.alvinodev.screenmatchpro.controller;
 
+import com.alvinodev.screenmatchpro.model.Serie;
+import com.alvinodev.screenmatchpro.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SerieController {
+    @Autowired
+    private SerieRepository repository;
 
     @GetMapping("/series")
-    public String showMessage(){
-        return "Este mensaje es desde showMessage";
+    public List<Serie> getAllSeries(){
+        return repository.findAll();
     }
 }
