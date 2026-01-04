a1913347bfef697d64bae41be53339f93c936095
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-04 14:33:19 -0400

    init: archivos base del proyecto

---
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/ApirestFirstappApplication.java
@@ -0,0 +1,13 @@
+package alvino.dev.apirest_firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestFirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestFirstappApplication.class, args);
+	}
+
+}
---
+++ apirest-firstapp/src/test/java/alvino/dev/apirest_firstapp/ApirestFirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package alvino.dev.apirest_firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestFirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
