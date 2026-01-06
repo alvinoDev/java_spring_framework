af6625461bc52c73f402fb0b3e40e71454022df6
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-06 15:16:15 -0400

    feat: Implementar paginación en la API de médicos utilizando Spring Data Pageable para mejorar la eficiencia y la experiencia del usuario.

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/controller/MedicoController.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/controller/MedicoController.java
@@ -6,10 +6,10 @@ import alvino.dev.apirest_firstapp.medico.Medico;
 import alvino.dev.apirest_firstapp.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +25,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListaMedico> listar() {
-        return medicoRepository.findAll().stream().map(DatosListaMedico::new).toList();
+    public Page<DatosListaMedico> listar(Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatosListaMedico::new);
     }
 }
