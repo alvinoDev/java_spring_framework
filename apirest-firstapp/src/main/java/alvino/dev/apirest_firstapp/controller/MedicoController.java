621354f2d53510e4c1994e8f9943faab0a9613b4
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-06 11:03:08 -0400

    feat: configurar Flyway para gestionar migraciones de base de datos, crea la tabla 'médicos' y asegura la gestión de transacciones en el controlador. Comentar la creacion automática de las tablas con Hibernate

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/controller/MedicoController.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/controller/MedicoController.java
@@ -4,6 +4,7 @@ import alvino.dev.apirest_firstapp.medico.DatosRegistroMedico;
 import alvino.dev.apirest_firstapp.medico.Medico;
 import alvino.dev.apirest_firstapp.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +14,7 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    @Transactional
     @PostMapping
     public void registrar(@RequestBody DatosRegistroMedico datos) {
         medicoRepository.save(new Medico(datos));
