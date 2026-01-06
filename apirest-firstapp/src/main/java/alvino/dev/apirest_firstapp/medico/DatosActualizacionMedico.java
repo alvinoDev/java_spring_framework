fc13829c1d77779a2d8d86c5689c2627fa970098
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-06 16:12:41 -0400

    feat: actualizar validacion, es obligatorio el ID

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/medico/DatosActualizacionMedico.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/medico/DatosActualizacionMedico.java
@@ -1,9 +1,10 @@
 package alvino.dev.apirest_firstapp.medico;
 
 import alvino.dev.apirest_firstapp.direccion.DatosDireccion;
+import jakarta.validation.constraints.NotNull;
 
 public record DatosActualizacionMedico(
-        Long id,
+        @NotNull Long id,
         String nombre,
         String telefono,
         DatosDireccion direccion
