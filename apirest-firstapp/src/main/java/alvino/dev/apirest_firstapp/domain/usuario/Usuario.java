dd08f933cb54c4ba0d3a73c9865d79d7aa773091
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-07 17:04:55 -0400

    feat: Configura la entidad Usuario y migración para la tabla de usuarios
    - Creación de la entidad JPA Usuario con Lombok para simplificar el código.
    - Configuración del ID autogenerado para la entidad Usuario.
    - Creación de la migración para la tabla de usuarios en la base de datos.
    - Implementación de hashing para contraseñas por seguridad.

---
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/domain/usuario/Usuario.java
@@ -0,0 +1,21 @@
+package alvino.dev.apirest_firstapp.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String contrasena;
+}
