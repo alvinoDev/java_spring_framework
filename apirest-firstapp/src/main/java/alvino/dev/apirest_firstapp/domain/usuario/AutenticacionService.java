4008abbc00dc632b34d8b3c37f0023d442eaf071
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-07 17:17:23 -0400

    feat: Implementar UsuarioRepository y AutenticacionService
    - Crea UsuarioRepository para interactuar con la base de datos.
    - Implementa AutenticacionService para gestionar la autenticación de usuarios.
    - Integra UsuarioRepository en AutenticacionService para buscar usuarios por nombre de usuario.

---
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/domain/usuario/AutenticacionService.java
@@ -0,0 +1,17 @@
+package alvino.dev.apirest_firstapp.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacionService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username);
+    }
+}
