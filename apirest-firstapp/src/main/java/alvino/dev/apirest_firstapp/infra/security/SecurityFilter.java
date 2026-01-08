2d73d0e27755f43d4a9701a37df229b381fabbed
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-08 11:11:52 -0400

    feat: Implementar autenticación y control de acceso en Spring Security
    - Configura URIs y permisos para usuarios autenticados y no autenticados.
    - Implementa filtro de seguridad para validar y autenticar usuarios mediante JWT.
    - Corrige errores en la validación del token y en el flujo de autenticación.
    - Fuerza la autenticación del usuario en Spring Security.

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/SecurityFilter.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package alvino.dev.apirest_firstapp.infra.security;
 
+import alvino.dev.apirest_firstapp.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +15,8 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Autowired
     private TokenService tokenService;
@@ -19,18 +24,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
+        if(tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByUsername(subject);
 
-        System.out.println("Subject : " + subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
 
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null) {
-            throw new RuntimeException("TOKEN JWT no enviado en el encabezado de Authorization");
+        if(authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
