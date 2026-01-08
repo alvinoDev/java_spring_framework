8ce12a0e696421b14f20004fbdd74b623da96399
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-08 10:59:08 -0400

    feat: Validar token JWT y extrae subject para control de acceso
    - Implementa método getSubject en TokenService para validar y extraer el usuario del token.
    - Integra TokenService en SecurityFilter para verificar el token en cada request.
    - Agrega pruebas para asegurar la correcta validación del token.

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/TokenService.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import alvino.dev.apirest_firstapp.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +33,18 @@ public class TokenService {
 
     private Instant fechaExpiracion() {
         return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-04:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API First App")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw  new RuntimeException("TOKEN JWT invalido o expirado!");
+        }
     }
 }
