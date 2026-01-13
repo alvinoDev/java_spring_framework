88dd291e57202922751da5d8507da2c94a951dce
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-13 10:38:32 -0400

    feat: Integra SpringDoc para la documentación automática de la API

--- apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/SecurityConfigurations.java
+++ apirest-firstapp/src/main/java/alvino/dev/apirest_firstapp/infra/security/SecurityConfigurations.java
@@ -25,9 +25,9 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( req -> {
-                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
-                    req.requestMatchers(HttpMethod.POST, "/register").permitAll();
-                    req.anyRequest().authenticated();
+                    req.requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
