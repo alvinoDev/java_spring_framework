c723353978e3e07cdfe3f10ef9974b0fb0859e15
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-13 12:21:28 -0400

    feat: Configura entorno de pruebas automatizadas con base de datos MySQL
    - Añadida anotación `@DataJpaTest` para pruebas de persistencia.
    - Añadida anotación `@AutoConfigureTestDatabase` para usar la base de datos MySQL real.
    - Creado archivo `application-test.properties` para la configuración de la base de datos de pruebas.
    - Añadida anotación `@ActiveProfiles("test")` para activar el perfil de pruebas.
    - Creada base de datos `volmed_api_test` para las pruebas.

---
+++ apirest-firstapp/src/test/java/alvino/dev/apirest_firstapp/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,20 @@
+package alvino.dev.apirest_firstapp.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void seleccionarMedicoAleatorioDisponibleEnLaFecha() {
+
+    }
+}
