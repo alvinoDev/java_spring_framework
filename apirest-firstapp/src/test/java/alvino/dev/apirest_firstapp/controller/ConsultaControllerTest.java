c321ad54baf8129ee223ad73ef5e27b2560d0bf9
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-14 12:07:22 -0400

    feat: Añadido test para endpoint de reserva de consultas con respuesta HTTP 200
    Se implementó un test para verificar el correcto funcionamiento del endpoint de reserva de consultas, asegurando que devuelve un HTTP 200 (OK) al recibir un JSON válido. Se utilizó JacksonTester para facilitar la creación de JSON y Mockito para simular la capa de servicio y evitar el acceso a la base de datos.

--- apirest-firstapp/src/test/java/alvino/dev/apirest_firstapp/controller/ConsultaControllerTest.java
+++ apirest-firstapp/src/test/java/alvino/dev/apirest_firstapp/controller/ConsultaControllerTest.java
@@ -1,23 +1,45 @@
 package alvino.dev.apirest_firstapp.controller;
 
+import alvino.dev.apirest_firstapp.domain.consulta.DatosDetalleConsulta;
+import alvino.dev.apirest_firstapp.domain.consulta.DatosReservaConsulta;
+import alvino.dev.apirest_firstapp.domain.consulta.ReservaDeConsultas;
+import alvino.dev.apirest_firstapp.domain.medico.Especialidad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<DatosReservaConsulta> datosReservaConsultaJT;
+
+    @Autowired
+    private JacksonTester<DatosDetalleConsulta> datosDetalleConsultaJT;
+
+    @MockitoBean
+    private ReservaDeConsultas reservaDeConsultas;
 
     @Test
     @DisplayName("Deberia devolver HTTP 400 cuando la request no tenga datos.")
@@ -26,5 +48,30 @@ class ConsultaControllerTest {
         var response = mockMvc.perform(post("/consultas")).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deberia devolver HTTP 200 cuando la request reciba un JSON valido.")
+    @WithMockUser
+    void reservar_escenario2() throws Exception {
+
+        var fecha = LocalDateTime.now().plusHours(1);
+        var especialidad = Especialidad.CARDIOLOGIA;
+        var datosDetalleEsperado = new DatosDetalleConsulta(null, 2l, 5l, fecha);
+        when(reservaDeConsultas.reservar(any())).thenReturn(datosDetalleEsperado);
+
+        var response = mockMvc.perform(post("/consultas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(datosReservaConsultaJT.write(
+                        new DatosReservaConsulta(2l, 5l, fecha, especialidad)
+                ).getJson()
+                ))
+                .andReturn().getResponse();
+        var jsonEsperado = datosDetalleConsultaJT.write(
+                datosDetalleEsperado
+        ).getJson();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
