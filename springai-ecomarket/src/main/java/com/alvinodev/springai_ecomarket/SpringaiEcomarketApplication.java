bf24c3737e400b2dd594823ee56ca004055cbc99
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-02-02 16:04:54 -0400

    init: agregar archivos base del proyecto

---
+++ springai-ecomarket/src/main/java/com/alvinodev/springai_ecomarket/SpringaiEcomarketApplication.java
@@ -0,0 +1,13 @@
+package com.alvinodev.springai_ecomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiEcomarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiEcomarketApplication.class, args);
+	}
+
+}
---
+++ springai-ecomarket/src/test/java/com/alvinodev/springai_ecomarket/SpringaiEcomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinodev.springai_ecomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiEcomarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
