baf835207d98424ad8188784a136de1fafad05a5
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-02-03 15:50:22 -0400

    feat(categorias): agregar parametros para hacer contar tokens

--- springai-ecomarket/src/main/java/com/alvinodev/springai_ecomarket/controller/CategorizadorDeProductosController.java
+++ springai-ecomarket/src/main/java/com/alvinodev/springai_ecomarket/controller/CategorizadorDeProductosController.java
@@ -56,4 +56,32 @@ public class CategorizadorDeProductosController {
 
         return Map.of("categoria", response);
     }
+
+    @GetMapping("/contartokens")
+    public Map<String, Object> categorizarConTokens(
+            @RequestParam(value = "producto") String producto
+    ) {
+        var pregunta = "Genera 5 productos ecológicos";
+        // 1. Realizamos la llamada completa
+        var response = this.chatClient.prompt(pregunta)
+                .user(producto)
+                .options(GoogleGenAiChatOptions.builder()
+                        .model("gemini-3-flash-preview")
+                        .temperature(0.5)
+                        .build())
+                .call()
+                .chatResponse(); // Obtenemos el objeto de respuesta completo
+
+        // 2. Extraemos los metadatos de uso
+        var metadata = response.getMetadata().getUsage();
+
+        return Map.of(
+                "respuesta", response.getResult().getOutput().getText(),
+                "uso", Map.of(
+                        "tokens_entrada", metadata.getPromptTokens(),
+                        "tokens_salida", metadata.getCompletionTokens(),
+                        "tokens_totales", metadata.getTotalTokens()
+                )
+        );
+    }
 }
